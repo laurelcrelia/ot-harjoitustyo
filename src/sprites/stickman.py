097559fa1aecdf85d9f99b647245cd6b26eac499
laurelcrelia <laurel.matt@helsinki.fi>
2022-04-05 16:40:56 +0300

    kuvat päivitetty

--- src/sprites/stickman.py
+++ src/sprites/stickman.py
@@ -3,11 +3,11 @@ import os
 
 dirname = os.path.dirname(__file__)
 
-class Stickman1(pygame.sprite.Sprite):
+class Stickman(pygame.sprite.Sprite):
     def __init__(self, x=0, y=0):
         super().__init__()
 
-        self.image = pygame.image.load(os.path.join(dirname,"..","assets","stickman1.png"))
+        self.image = pygame.image.load(os.path.join(dirname,"..","assets","stickman.png"))
 
         self.rect = self.image.get_rect()
 
