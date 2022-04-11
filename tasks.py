7da772020a8777714d21fc4c0bf31a0491abafe3
laurelcrelia <laurel.matt@helsinki.fi>
2022-04-11 19:07:06 +0300

    invoke lint ja format

--- tasks.py
+++ tasks.py
@@ -16,3 +16,10 @@ def coverage(ctx):
 def coverage_report(ctx):
 	ctx.run("coverage html", pty=True)
 
+@task
+def format(ctx):
+	ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+@task
+def lint(ctx):
+	ctx.run("pylint src", pty=True)
