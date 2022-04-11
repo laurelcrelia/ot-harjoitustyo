a7818a1ec5ebd927e9413804852b4d1aa9d2012b
laurelcrelia <laurel.matt@helsinki.fi>
2022-04-12 00:22:14 +0300

    luokkia selkeytetty

---
+++ src/game_loop.py
@@ -0,0 +1,40 @@
+import pygame
+
+class GameLoop:
+    def __init__(self, level, renderer, cell_size, clock):
+        self.level = level
+        self.renderer = renderer
+        self.cell_size = cell_size
+        self.clock = clock
+    
+    def start(self):
+        #self.menu_initialization()
+        while True:
+            if self.movements() == False:
+                break
+
+            self.render()
+
+            self.clock.tick(60)     
+
+    def movements(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.level.move_stickman(x=-50)
+                if event.key == pygame.K_RIGHT:
+                    self.level.move_stickman(x=50)
+                if event.key == pygame.K_UP:
+                    self.level.move_stickman(y=-50)
+                if event.key == pygame.K_DOWN:
+                    self.level.move_stickman(y=50)
+            elif event.type == pygame.QUIT:
+                return False
+        
+    def render(self):
+        self.renderer.render()
+
+    # def menu_initialization(self):
+    #     self.screen.fill(0,0,0)
+
+
--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 import pygame
 from level import Level
-
+from game_loop import GameLoop
+from renderer import Renderer
 
 def main():
     """Luo peliruudukon ja mahdollistaa ruudukossa liikkumisen"""
@@ -16,34 +17,19 @@ def main():
 
     size = 50
 
-    level = Level(level_1, size)
-
     screen = pygame.display.set_mode((400, 400))
     pygame.display.set_caption("Stickman")
-
-    pygame.init()
-    level.all_sprites.draw(screen)
 
     clock = pygame.time.Clock()
     clock.tick(60)
 
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    level.move_stickman(x=-50)
-                if event.key == pygame.K_RIGHT:
-                    level.move_stickman(x=50)
-                if event.key == pygame.K_UP:
-                    level.move_stickman(y=-50)
-                if event.key == pygame.K_DOWN:
-                    level.move_stickman(y=50)
-            elif event.type == pygame.QUIT:
-                run = False
+    level = Level(level_1, size)
+    renderer = Renderer(screen, level)
+    game_loop = GameLoop(level, renderer, size, clock)
 
-        pygame.display.update()
-        level.all_sprites.draw(screen)
+
+    pygame.init()
+    game_loop.start()
 
 
 if __name__ == "__main__":
