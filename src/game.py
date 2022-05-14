5f8303824c08f11ef5a765d44c0316f40728501f
laurelcrelia <laurel.matt@helsinki.fi>
2022-05-14 19:31:59 +0300

    taso 5 luotu

--- src/game.py
+++ src/game.py
@@ -3,7 +3,7 @@ from level import Level
 from game_loop import GameLoop
 from renderer import Renderer
 from menu import MenuView
-from levels import LEVELS
+from levels import LEVELS, SCREEN_X, SCREEN_Y
 
 
 class Game:
@@ -11,43 +11,27 @@ class Game:
         """Luokan konstruktori. Alustaa pelin käynnistystä varten tarvittavat parametrit.
         """
         self.size = 50
-        self.index = 0
         self.screen = pygame.display.set_mode((400, 450))
         pygame.display.set_caption("Labyrinth")
         self.clock = pygame.time.Clock()
         self.clock.tick(60)
 
-
-    def change_level(self):
-        self.index += 1
-
     def main(self):
         """Luokka käynnistää pygamen ja luo Gameloop-luokan instanssin."""
         pygame.init()
-        level_1 = Level(LEVELS[self.index], self.size)
-        renderer_1 = Renderer(self.screen, level_1)
-        menu = MenuView(self.screen)
-        game_loop_1 = GameLoop(level_1, self.screen, renderer_1,
+        level_1 = Level(LEVELS[0], self.size)
+        renderer_1 = Renderer(pygame.display.set_mode((SCREEN_X[0], SCREEN_Y[0])), level_1)
+        menu = MenuView(pygame.display.set_mode((SCREEN_X[0], SCREEN_Y[0])))
+        game_loop_1 = GameLoop(level_1, pygame.display.set_mode((SCREEN_X[0], SCREEN_Y[0])), renderer_1,
                                self.size, self.clock, menu)
         game_loop_1.start()
-        self.change_level()
-        level_2 = Level(LEVELS[self.index], self.size)
-        renderer_2 = Renderer(self.screen, level_2)
-        game_loop_2 = GameLoop(level_2, self.screen, renderer_2,
-                               self.size, self.clock, menu)
-        game_loop_2.start_2()
-        self.change_level()
-        level_2 = Level(LEVELS[self.index], self.size)
-        renderer_2 = Renderer(self.screen, level_2)
-        game_loop_2 = GameLoop(level_2, self.screen, renderer_2,
-                               self.size, self.clock, menu)
-        game_loop_2.start_2()
-        self.change_level()
-        level_2 = Level(LEVELS[self.index], self.size)
-        renderer_2 = Renderer(self.screen, level_2)
-        game_loop_2 = GameLoop(level_2, self.screen, renderer_2,
-                               self.size, self.clock, menu)
-        game_loop_2.start_2()
+        for i in range(1,5):
+            level_2 = Level(LEVELS[i], self.size)
+            renderer_2 = Renderer(pygame.display.set_mode((SCREEN_X[i], SCREEN_Y[i])), level_2)
+            game_loop_2 = GameLoop(level_2, pygame.display.set_mode((SCREEN_X[i], SCREEN_Y[i])), renderer_2,
+                                self.size, self.clock, menu)
+            game_loop_2.start_2()
+
 
 if __name__ == "__main__":
     GAME = Game()
